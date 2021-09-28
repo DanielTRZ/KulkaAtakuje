db421980327bad81e9f399826c7a60304745a35d
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-09-28 15:26:01 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -57,7 +57,7 @@ var can = document.getElementById('gra');
 			}
         }
 		
-		function Moneta (px, py, pszer, pwys)
+                function Moneta (px, py, pszer, pwys)
 		{
 		    this.x = px;
 			this.y = py;
