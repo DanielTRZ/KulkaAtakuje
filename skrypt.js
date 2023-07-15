e7470346186a83d93bed22167b01424769043327
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2023-07-15 17:21:46 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -60,7 +60,7 @@
 		{
 		    for (var i = 0;i<plat.length;i++)
 			{
-			 ctx.drawImage(GrafikaPlatformy, plat[i].x, plat[i].y, plat[i].szer, plat[i].wys);
+		         ctx.drawImage(GrafikaPlatformy, plat[i].x, plat[i].y, plat[i].szer, plat[i].wys);
 			}
                 }
 		
