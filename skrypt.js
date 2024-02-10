c1c7e2e53af419e480ecaa824a6dee0880c49d63
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-02-10 20:29:49 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -116,7 +116,7 @@
 		        for (var i=0; i<plat.length; i++)
                 {
 			if(yPos + wysPos > plat[i].y &&
-                 yPos + 0.8*wysPos < plat[i].y &&
+                        yPos + 0.8*wysPos < plat[i].y &&
                  xPos + szerPos/2 > plat[i].x &&
                  xPos + szerPos/2 <plat[i].x + plat[i].szer)
                 {
