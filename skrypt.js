b8a8cdb167904c96d6ec9245e14ef636559edc34
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-04-01 19:22:01 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -172,12 +172,12 @@
 		{
 	        	for (var i=0; i<mon.length; i++)
             {
-		 if(yPos < mon[i].y + mon[i].wys/2 &&
+		        if(yPos < mon[i].y + mon[i].wys/2 &&
                         yPos + wysPos > mon[i].y + mon[i].wys/2 &&
-                 xPos < mon[i].x + mon[i].szer/2&&
-                 xPos + szerPos > mon[i].x + mon[i].szer/2)
+                        xPos < mon[i].x + mon[i].szer/2&&
+                        xPos + szerPos > mon[i].x + mon[i].szer/2)
                 {
-                 mon[i].czywidoczna = false;
+                        mon[i].czywidoczna = false;
                 }
 			}
 		}
