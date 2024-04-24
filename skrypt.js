6d2c90e4aa352929221f9bd99feee20f3b742913
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-04-24 16:02:35 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -79,8 +79,8 @@
 		    {
 		    if (mon[i].czywidoczna == true)
 		    {
-		         ctx.drawImage(GrafikaMonety, mon[i].x, mon[i].y, mon[i].szer, mon[i].wys);
-			}
+		    ctx.drawImage(GrafikaMonety, mon[i].x, mon[i].y, mon[i].szer, mon[i].wys);
+		    }
 			}
 		}
 		
