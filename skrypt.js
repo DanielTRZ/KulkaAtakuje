ea38b6b4f56b63df2e7d74f86e628f0e8689bfc4
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-04-28 15:49:07 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -100,7 +100,7 @@
 		{
 		    for (var i = 0;i<przesz.length;i++)
 	            {
-		        if (przesz[i].czywidoczna == true)
+		    if (przesz[i].czywidoczna == true)
 		        {
 			 ctx.drawImage(przesz[i].GrafikaPrzeszkody, przesz[i].x, przesz[i].y, przesz[i].szer, przesz[i].wys);
 			}
