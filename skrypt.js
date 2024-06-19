f045b4f6ed0f739e13e2a64922678fdda3dcfdab
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-19 13:53:04 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,6 +1,6 @@
 var can = document.getElementById('gra');
 var ctx = can.getContext('2d');
-		var GrafikaPlatformy = new Image();
+var GrafikaPlatformy = new Image();
 		GrafikaPlatformy.src = 'platforma.png';
 		var plat = [];
 		plat[0] = new Platforma(0,500,300, 80);
