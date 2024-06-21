846ccb291b4e912e9cae15b94324341076e4549a
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-21 17:47:22 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -35,7 +35,7 @@ przesz[0] = new Przeszkoda(40, 170,35, 40, 95, 'grzyb.png');
 przesz[1] = new Przeszkoda(420, 340,35, 40, 10, 'ogień.png');
 przesz[2] = new Przeszkoda(320, 25,35, 40, 10, 'ogień.png');
 przesz[3] = new Przeszkoda(540, 120,35, 40, 10, 'ogień.png');
-                przesz[4] = new Przeszkoda(790,50,35, 40, 10, 'ogień.png');
+przesz[4] = new Przeszkoda(790,50,35, 40, 10, 'ogień.png');
                 przesz[5] = new Przeszkoda(1100,90,35, 40, 10, 'ogień.png');
       		
 		var Grafikapostaci = new Image();
