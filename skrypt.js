f07ee8d2d4fa8c8c42c3afdb8a65b41e7fb7cffb
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-23 16:19:29 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -108,7 +108,7 @@ ctx.drawImage(przesz[i].GrafikaPrzeszkody, przesz[i].x, przesz[i].y, przesz[i].s
 }
 		
 var dy = 0;
-		function grawitacja()
+function grawitacja()
 		{
 	             if (dy >= 0)
 	             {
