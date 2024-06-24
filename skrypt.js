b1003f30d386bb747fb58dd6e9b710af16b0ef37
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-24 12:29:28 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -120,7 +120,7 @@ yPos + 0.8*wysPos < plat[i].y &&
 xPos + szerPos/2 > plat[i].x &&
 xPos + szerPos/2 <plat[i].x + plat[i].szer)
 {
-                        dy = 0;
+dy = 0;
                 }
 			}
 			}
