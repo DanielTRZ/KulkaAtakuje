ec4b10bb3ee5373e86b6a5fc27011229fb5d11bc
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-26 01:18:03 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -171,7 +171,7 @@ dx = 0;
 function kolizjaZMoneta()
 {
 for (var i=0; i<mon.length; i++)
-            {
+{
 		        if(yPos < mon[i].y + mon[i].wys/2 &&
                         yPos + wysPos > mon[i].y + mon[i].wys/2 &&
                         xPos < mon[i].x + mon[i].szer/2&&
